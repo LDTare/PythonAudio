6ff8ed8159ba3f9585649823051ae6316fa9e676
LDTare <61979444+LDTare@users.noreply.github.com>
2022-04-24 23:05:21 -0600

    Comentarios

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from tkinter import filedialog
 import numpy as np
 import sounddevice as sd
 import soundfile as sf
-import subprocess
+import subprocess #Libreria para convertir los archivos MP3 a WAV
 
 root = Tk()
 
@@ -16,130 +16,132 @@ Ruta = ""
 
 def SeleccionArchivos(): #Funcion para seleccionar archivos
     global filename
+    #Filtro para seleccionar unicamente archivos MP3, WAV y OGG
     filename = filedialog.askopenfilename(initialdir="D:/", title="Selecciona un archivo de audio", filetypes=(("Archivos MP3", "*.MP3"), ("Archivos WAV", "*.WAV"),("Archivos OGG", "*.OGG")))
     global Ruta
+    #Almacenamiento de la ruta en una variable
     Ruta = filename
 
-def PlayArchivo():
-    #Reproducir audio
-    subprocess.call(['ffmpeg', '-y' , '-i', Ruta, 'test.wav'])
+def PlayArchivo(): #Reproducir el archivo
+    subprocess.call(['ffmpeg', '-y' , '-i', Ruta, 'test.wav']) #Conversion a WAV
     x, fs = sf.read('test.wav', dtype='float32')
-    print('reproduciendo musica original')
+    print('En reproducción: Canción original')
     sd.play(x,fs)
 
-def StopArchivo():
+def StopArchivo(): #Deneter reproducción del archivo
     sd.stop()
 
-def InvertirCanales():
-    subprocess.call(['ffmpeg','-y','-i', Ruta, 'test.wav'])
+def InvertirCanales(): #Cambiar el canal 1 al 2 y el canal 2 al 1
+    subprocess.call(['ffmpeg','-y','-i', Ruta, 'test.wav']) #Conversion a WAV
     x, fs = sf.read('test.wav', dtype='float32')
-    can1 = x[0:len(x), 0]
-    can1 = can1.reshape(len(can1), 1)
-    # print('Canal 1: ',can1)
-    can2 = x[0:len(x), 1]
-    can2 = can2.reshape(len(can2), 1)
-    # print('Canal 2: ',can2)
 
-    y = np.append(can2, can1, axis=1)
-    print('Canales invertidos: ', y)
-    print('Frecuencia de muestreo: ', fs)
+    canal1 = x[0:len(x), 0] #Almacenamiento de los valores en la fila 1 del muestreo
+    canal1 = canal1.reshape(len(canal1), 1) #Se cambia la dimencion del arreglo
+ 
+    canal2 = x[0:len(x), 1] #Almacenamiento de los valores en la fila 2 del muestreo
+    canal2 = canal2.reshape(len(canal2), 1) #se cambia la dimencion del arreglo
 
-    print('reproduciendo canales cambiados')
+
+    y = np.append(canal2, canal1, axis=1) #Cambio de posicion entre canales
+
+    print('En reproducción: Canción alterada')
     sd.play(y, fs)
 
 
-def ReflejarCanales():
-    subprocess.call(['ffmpeg', '-y', '-i', Ruta, 'test.wav'])
+def ReflejarCanales(): #Reproducir los canales en sentido inverso
+    subprocess.call(['ffmpeg', '-y', '-i', Ruta, 'test.wav']) #Conversion a WAV
     x, fs = sf.read('test.wav', dtype='float32')
-    can1 = np.array(x[0:len(x), 0])
-    can1 = can1.reshape(len(can1), 1)
 
-    can2 = np.array(x[0:len(x), 1])
-    can2 = can2.reshape(len(can2), 1)
+    canal1 = x[0:len(x), 0] #Almacenamiento de los valores en la fila 1 del muestreo
+    canal1 = canal1.reshape(len(canal1), 1) #Se cambia la dimencion del arreglo
+ 
+    canal2 = x[0:len(x), 1] #Almacenamiento de los valores en la fila 2 del muestreo
+    canal2 = canal2.reshape(len(canal2), 1) #se cambia la dimencion del arreglo
 
-    y = np.append(can1, can2, axis=1)
-    y = y[::-1]
-    print('Canales Reflejados: ', y)
-    print('Frecuencia de muestreo: ', fs)
 
-    print('reproduciendo reflexion de canales')
+    y = np.append(canal1, canal2, axis=1) #Se vuelven a unir los canales
+
+    y = y[::-1] #Se realiza el reflejo de los datos
+
+    print('En reproducción: Canción alterada')
     sd.play(y, fs)
 
-def Alteracion():
-    subprocess.call(['ffmpeg', '-y', '-i', Ruta, 'test.wav'])
+
+def Alteracion(): #Aumento de la intensidad del archivo
+    subprocess.call(['ffmpeg', '-y', '-i', Ruta, 'test.wav']) #Conversion a WAV
     x, fs = sf.read('test.wav', dtype='float32')
 
-    Data = float(valor.get())
-    Data = 5 + Data/100
+    Data = float(valor.get()) #Lectura de la cantidad para aumentar desde el input
+    Data = 5 + Data/100 #Limite de aumento nota peronal: duele mucho cuando no lo tiene mas si llega a 100
 
     print(Data)
 
-    can1 = np.array(x[0:len(x), 0])
-    can2 = np.array(x[0:len(x), 1])
+    canal1 = np.array(x[0:len(x), 0]) #Almacenamiento de los valores en la fila 1 del muestreo
+    canal2 = np.array(x[0:len(x), 1]) #Almacenamiento de los valores en la fila 2 del muestreo
 
-    Tmp = np.array([Data])
-    print(Tmp)
-    print(can1)
+    Tmp = np.array([Data]) #Creación de un array temporal para la convolucion
 
-    cntemp1 = np.convolve(can1, Tmp)
-    cntemp2 = np.convolve(can2, Tmp)
+    cntemp1 = np.convolve(canal1, Tmp) #Convolución del canal 1 con el aumento
+    cntemp2 = np.convolve(canal2, Tmp) #Convolución del canal 2 con el aumento
 
-    cntemp1 = cntemp1.reshape(len(cntemp1),1)
-    cntemp2 = cntemp2.reshape(len(cntemp2), 1)
+    cntemp1 = cntemp1.reshape(len(cntemp1),1)  #se cambia la dimencion del arreglo
+    cntemp2 = cntemp2.reshape(len(cntemp2), 1)  #se cambia la dimencion del arreglo
 
-    y = np.append(cntemp1, cntemp2, axis=1)
+    y = np.append(cntemp1, cntemp2, axis=1) #Se vuelven a unir los canales
 
+    print('En reproducción: Canción aumentada')
     sd.play(y,fs)
 
 def Decremento():
-    subprocess.call(['ffmpeg', '-y', '-i', Ruta, 'test.wav'])
+    subprocess.call(['ffmpeg', '-y', '-i', Ruta, 'test.wav']) #Conversion a WAV
     x, fs = sf.read('test.wav', dtype='float32')
 
-    Data = float(valor.get())
-    Data = 1 - Data / 100
+    Data = float(valor.get()) #Lectura de la cantidad para la reducción desde el input
+    Data = 1 - Data / 100 #Limite de aumento nota peronal: duele mucho cuando no lo tiene mas si llega a 100
 
-    print(Data)
+    canal1 = np.array(x[0:len(x), 0]) #Almacenamiento de los valores en la fila 1 del muestreo
+    canal2 = np.array(x[0:len(x), 1]) #Almacenamiento de los valores en la fila 2 del muestreo
 
-    can1 = np.array(x[0:len(x), 0])
-    can2 = np.array(x[0:len(x), 1])
+    Tmp = np.array([Data]) #Creación de un array temporal para la convolucion
 
-    Tmp = np.array([Data])
-    print(Tmp)
-    print(can1)
+    cntemp1 = np.convolve(canal1, Tmp) #Convolución del canal 1 con la reduccion
+    cntemp2 = np.convolve(canal2, Tmp) #Convolución del canal 2 con la reduccion
+ 
+    cntemp1 = cntemp1.reshape(len(cntemp1), 1) #se cambia la dimencion del arreglo
+    cntemp2 = cntemp2.reshape(len(cntemp2), 1) #se cambia la dimencion del arreglo
 
-    cntemp1 = np.convolve(can1, Tmp)
-    cntemp2 = np.convolve(can2, Tmp)
-
-    cntemp1 = cntemp1.reshape(len(cntemp1), 1)
-    cntemp2 = cntemp2.reshape(len(cntemp2), 1)
-
-    y = np.append(cntemp1, cntemp2, axis=1)
-
+    y = np.append(cntemp1, cntemp2, axis=1) #Se vuelven a unir los canales
+ 
+    print('En reproducción: Canción disminuida')
     sd.play(y, fs)
 
-def Recorte():
-    subprocess.call(['ffmpeg', '-y', '-i', Ruta, 'test.wav'])
+def Recorte(): #Extraccióno de fragmento
+    subprocess.call(['ffmpeg', '-y', '-i', Ruta, 'test.wav']) #Conversion a WAV
     x, fs = sf.read('test.wav', dtype='float32')
 
-    i = int(DesdeINP.get())
-    f = int(HastaINP.get())
-    inicio = i * fs
-    final = f * fs
-    # restriccion
-    if inicio < 0:
-        inicio = 0
-    if final > len(x):
+    i = int(DesdeINP.get()) #Conversion a entero de la entrada para el inicio del corte de segmento
+    f = int(HastaINP.get()) #Conversion a entero de la entrada para el fin del corte de segmento
+
+    inicio = i * fs # Calculo de la frecuencia inicial
+    final = f * fs # Calculo de la frecuencia final 
+
+    if inicio < 0: #Restriccion para evitar inicar en una canitdad menor a la frecuencia
+        inicio = 0 
+    if final > len(x): #Restriccion para evitar terminar en una cantidad mayor a la frecuencia
         final = len(x)
-    print(len(x), inicio, final)
-    can1 = x[inicio:final, 0]
-    can1 = can1.reshape(len(can1), 1)
-    can2 = x[inicio:final, 1]
-    can2 = can2.reshape(len(can2), 1)
-    y = np.append(can1, can2, axis=1)
-    print('reproduciendo extracción:')
+
+    canal1 = x[inicio:final, 0] #Obtencion de muestras de la señal x desde el calculo de la frecuencia inicial hasta la final para el canal 1
+    canal1 = canal1.reshape(len(canal1), 1) #se cambia la dimencion del arreglo
+
+    canal2 = x[inicio:final, 1] #Obtencion de muestras de la señal x desde el calculo de la frecuencia inicial hasta la final para el canal 2
+    canal2 = canal2.reshape(len(canal2), 1) #se cambia la dimencion del arreglo
+
+    y = np.append(canal1, canal2, axis=1) #Se vuelven a unir los canales
+
+    print('En reproducción: Canción recortada')
     sd.play(y, fs)
 
-def Efecto():
+def Efecto(): #Efecto prueba
     subprocess.call(['ffmpeg', '-y', '-i', Ruta, 'test.wav'])
     x, fs = sf.read('test.wav', dtype='float32')
 
